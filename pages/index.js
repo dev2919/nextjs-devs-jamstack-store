b0562e4b3bfab43a30548be38d02f5929e9e5e84
devesh_pawar <devesh@iaspire.tech>
2022-03-26 12:52:38 +0530

    Testing code base to vercel

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import { getProductsInCollection } from '../adapters/shopify'
 import ProductList from '../components/productList/ProductList'
 import Pagination from '../components/pagination/Pagination'
 
-const Home: NextPage = ({ products }) => {
+function Home({ products }){
   return (
     <div className="text-3xl">
       <ProductList products={products}></ProductList>
