d9a75df6e5dd20bf7b9b4bb913e59d707d397d15
dev2919 <48598352+dev2919@users.noreply.github.com>
2022-07-29 12:32:18 +0000

    category fallback fixes

--- pages/index.js
+++ pages/index.js
@@ -62,5 +62,6 @@ export async function getStaticProps() {
 
   return {
     props: { products, collectionList, variants }, // will be passed to the page component as props
+    revalidate: 10
   }
 }
