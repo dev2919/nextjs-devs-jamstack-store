6c431f0d462d579b2fb0d7abcd76f59c085a9c83
dev2919 <48598352+dev2919@users.noreply.github.com>
2022-08-02 12:33:47 +0000

    ui fixes and video support added

--- adapters/shopify.js
+++ adapters/shopify.js
@@ -308,7 +308,7 @@ export async function getProduct(handle) {
             ... on MediaImage{
               image{
                 url
-                altText
+                
               }
             }
           }
